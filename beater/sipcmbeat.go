d09e845273880c2f23f66ff456d1bed6b9d0f5b0
Andrei Pelinescu-Onciul <andrei@iptel.org>
2020-11-04 20:18:29 +0100

    copyright spelling fix

--- beater/sipcmbeat.go
+++ beater/sipcmbeat.go
@@ -1,4 +1,4 @@
-// // Copyright 2019-2020 Intuitive Labs Gmbh. All rights reserved.
+// // Copyright 2019-2020 Intuitive Labs GmbH. All rights reserved.
 // //
 // // Use of this source code is governed by source-available license
 // // that can be found in the LICENSE file in the root of the source
