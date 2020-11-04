1c42560ebd773118be66c55e2e1c8102f27d7a6b
Andrei Pelinescu-Onciul <andrei@iptel.org>
2020-11-04 19:16:41 +0100

    copyright notice and license reference

--- beater/sipcmbeat.go
+++ beater/sipcmbeat.go
@@ -1,3 +1,9 @@
+// // Copyright 2019-2020 Intuitive Labs Gmbh. All rights reserved.
+// //
+// // Use of this source code is governed by source-available license
+// // that can be found in the LICENSE file in the root of the source
+// // tree.
+
 package beater
 
 import (
