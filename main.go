cf79ba7968a15c03634b6321791a57bae0761494
Andrei Pelinescu-Onciul <andrei@iptel.org>
2021-02-20 11:19:48 +0100

    main: cleanup + report error
    - remove unused code comments
    - in case of Execute() failure print error (although at least in
    case of beat.New() failure it will exit directly without reaching
    this point)

--- main.go
+++ main.go
@@ -1,32 +1,18 @@
 package main
 
 import (
-	//	"fmt"
+	"fmt"
 	"os"
 
-	// "github.com/intuitivelabs/sipcallmon"
-	//"github.com/intuitivelabs/sipcmbeat/beater"
 	"github.com/intuitivelabs/sipcmbeat/cmd"
 
 	_ "github.com/intuitivelabs/sipcmbeat/include"
 )
 
 func main() {
-	/*
-		cfg := sipcallmon.DefaultConfig
-		cfg.Iface = "eth0"
-		cfg.BPF = "port 5060"
-		cfg.Verbose = true
-		bt, err := beater.New(nil, nil)
-		if err != nil {
-			fmt.Printf("error: cfg: %v\n", err)
-			os.Exit(1)
-		}
-		bt.(*beater.Sipcmbeat).Config = cfg
-		bt.Run(nil)
-		//sipcallmon.Run(&cfg)
-	*/
 	if err := cmd.RootCmd.Execute(); err != nil {
+		// does not seem to be ever reached (os.Exit() on error from New?)
+		fmt.Fprintf(os.Stderr, "ERROR: %+v\n", err)
 		os.Exit(1)
 	}
 }
